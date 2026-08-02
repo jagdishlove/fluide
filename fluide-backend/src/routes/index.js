60d0124ef02049d3879a8dd1fcdfb78832564b81
jagdishlove <jagdish.funn@gmail.com>
2026-08-02 23:31:29 +0100

    updated backend health api

--- fluide-backend/src/routes/index.js
+++ fluide-backend/src/routes/index.js
@@ -5,4 +5,7 @@ const router = express.Router();
 const userRoute = require("./user.route");
 
 router.use("/", userRoute);
+router.use("/health", (req, res) => {
+  res.status(200).json({ message: "Server is running" });
+});
 module.exports = router;
